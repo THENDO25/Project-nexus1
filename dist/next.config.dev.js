6a8ecf55134083b14dc681d0822a49c4f1a00fbd
THENDO25 <thendomukwevho1125@gmail.com>
2025-03-09 20:18:23 +0200

    update

---
+++ dist/next.config.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+/** @type {import('next').NextConfig} */
+var nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+};
+var _default = nextConfig;
+exports["default"] = _default;
+//# sourceMappingURL=next.config.dev.js.map
